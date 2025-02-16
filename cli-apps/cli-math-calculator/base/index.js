5ccb5a15723dee817da12b72cae1beb47ffd5551
PrincessBz <162399259+PrincessBz@users.noreply.github.com>
2025-02-15 16:07:00 -0900

    Add the class work done for the files calculator. Added colete file for hello world

--- cli-apps/cli-math-calculator/base/index.js
+++ cli-apps/cli-math-calculator/base/index.js
@@ -1,43 +1,100 @@
 #!/usr/bin/env node
 
-const process = require('node:process');
+const process = require("node:process");
 
 /**
  * Perform basic arithmetic operations
- * 
- * @param {number} a First operand
+ *
+ * @param {number[]} operator The numbers on which to perform operation
  * @param {string} operator The mathematical operator (+, -, *, /)
- * @param {number} b Second operand
  * @returns The result of the operation
  */
-function calculate(a, operator, b) {
+function calculate(operator, numbers) {
+  let answer = numbers[0];
+  for (let i = 1; i < numbers.length; i++) {
+    switch (operator) {
+      case "+":
+        answer = answer + numbers[i];
+        break;
 
+      case "-":
+        answer = answer - numbers[i];
+        break;
+
+      case "/":
+        answer = answer / numbers[i];
+        break;
+
+      case "*":
+        answer = answer * numbers[i];
+        break;
+    }
+  }
+
+  return answer;
 }
 
 /**
  * Parses and evaluates chained operations
- * 
+ *
  * @param {string[]} args Array of operands and operators
  * @returns The final result after evaluating all operations
  */
-function evaluateChainedOperations(args) {
-    
-}
+function evaluateChainedOperations(args) {}
 
 /**
  * Prints the help message
  */
 function printHelpMessage() {
-    console.log(``);
+  console.log(`Help`);
 }
 
 /**
  * Handles the arguments provided to the application by the user.
- * 
+ *
  * @param {string[]} userArguments The arguments provided by the user
  */
 function handleArguments(userArguments) {
+  if (userArguments.length < 3) {
+    printHelpMessage();
+    return;
+  }
 
+  let operator;
+
+  switch (userArguments[0]) {
+    case "add":
+      operator = "+";
+      break;
+
+    case "subtract":
+      operator = "-";
+      break;
+
+    case "multiply":
+      operator = "*";
+      break;
+
+    case "divide":
+      operator = "/";
+      break;
+
+    default:
+      printHelpMessage();
+      return;
+  }
+
+  let numbers = [];
+  for (let i = 1; i < userArguments.length; i++) {
+    let parsedValue = parseFloat(userArguments[i]);
+    if (isNaN(parsedValue)) {
+      console.log("Error, the provided value is not a valid number");
+      printHelpMessage();
+      return;
+    }
+    numbers.push(parsedValue);
+  }
+  console.log(`The answer is: ${calculate(operator, numbers)}`);
 }
 
 handleArguments(process.argv.slice(2));
--- cli-apps/hello-world-app/base/index.js
+++ cli-apps/hello-world-app/base/index.js
@@ -1,5 +1,32 @@
 #!/usr/bin/env node
 
-const process = require('node:process');
+const process = require("node:process");
 const arguments = process.argv.slice(2);
+//node index.js --greet John
+//Hello John!
 
+let name = "World";
+
+for (let i = 0; i < arguments.length; i++) {
+  if (arguments[i] === "--greet") {
+    name = arguments[i + 1];
+  } else if (
+    arguments[i] === "--help" ||
+    arguments[i] === "-h" ||
+    arguments[i] === "help"
+  ) {
+    console.log(`
+        Usage:
+        --greet <name>   Print 'Hello <name>'
+        --help           Prints the help message
+
+        `);
+    return;
+  }
+}
+
+if (name === undefined) {
+  name = "World";
+}
+
+console.log(`Hello ${name}`);
